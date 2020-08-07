dd151a47d12bf4b592cb471389cfcab71ad5911a
harroshan <harroshan@users.noreply.github.com>
2020-08-07 21:24:54 +0530

    Pet Clinic Interfaces

---
+++ pet-clinic-data/src/main/java/harroshan/spring/petclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package harroshan.spring.petclinic.services;
+
+import harroshan.spring.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
