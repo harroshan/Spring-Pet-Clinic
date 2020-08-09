c24641eb06cfbec2b46b3925e4ebda1e31a4346c
harroshan <harroshan@users.noreply.github.com>
2020-08-09 12:50:25 +0530

    Refactoring to Common CRUD Service. Closes #5

--- pet-clinic-data/src/main/java/harroshan/spring/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/harroshan/spring/petclinic/services/OwnerService.java
@@ -2,13 +2,8 @@ package harroshan.spring.petclinic.services;
 
 import harroshan.spring.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 
 }
