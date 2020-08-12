0cd0a74c12aa8515b0953036f51e00ba9c42c616
harroshan <harroshan@users.noreply.github.com>
2020-08-12 19:54:08 +0530

    Update Map Impl

--- pet-clinic-web/src/main/java/harroshan/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/harroshan/spring/petclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Harroshan");
         owner1.setLastName("Makkar");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Michael");
         owner2.setLastName("Weston");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Max");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
 
